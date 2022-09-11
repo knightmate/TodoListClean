f50480795050f48bcba9425eed9ec96486bbd28c
MacMini <macmini@MacMinis-Mac-mini.local>
2022-09-11 21:01:18 +0530

    list save locally in cookies added

--- script.js
+++ script.js
@@ -4,38 +4,50 @@ const newListForm=document.querySelector("[data-new-list-form]")
 const newListInput=document.querySelector("[data-new-list-input");
 
 
+const LOCAL_STORAGE_KEY="LOCAL_STORAGE_KEY";
+console.log('fdf',JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)))
+ 
+const lists=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
 
-const lists=[];
-
-
-
+console.log("listInstart",lists)
+renderTask(lists);
 newListForm.addEventListener('submit',(e)=>{
 
 
     e.preventDefault();
     const listName=newListInput.value;
-    if(listName==null || listName=="")return;
+    if(!listName || listName==null || listName=="")return;
      newListInput.value=null
 
-    const newListItem={
+      const newListItem={
         name:listName,
         id:Date.now().toString()
     };
     lists.push(newListItem);
 
      renderTask(lists);
-     
+     console.log('listItems',lists)
+     saveList(lists)
 
-})
+});
+
+
+const saveList=(lists)=>{
+
+     localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(lists))
+
+};
 
 function renderTask(lists){
+    if(!lists.length)return
        clearListUI(listContainer);
+       console.log('rendertask',lists)
       lists.forEach((list=>{
         const listElement=document.createElement('li');
         listElement.id=list.id;
         listElement.innerText=list.name;
         listElement.classList.add('list-name');
-       console.log('list',listContainer)
+      // console.log('list',listContainer)
         listContainer.appendChild(listElement);
       }))
 
@@ -45,5 +57,5 @@ function clearListUI(element){
      
     while(element.firstChild){
         element.removeChild(element.firstChild)
-    }
+    };
 }
